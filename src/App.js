d37f535fb09b4a578f0f31f74381187b283ec0dc
Tim Krofecheck <tkrofecheck@gmail.com>
2017-10-30 14:43:58 -0400

    update restaurant results layout

--- src/App.js
+++ src/App.js
@@ -10,12 +10,18 @@ import greySeal from './images/seals/Seal_of_New_York_City_BW.png';
 import './App.css';
 
 const apiKey = 'GC25gGvU068FNzk16wkfN8vK6JmzsKfk6BsYzhpb';
+
 class Grade extends Component {
 	inspectionDate(inspection) {
 		if (typeof inspection !== 'undefined') {
+			var inspectionDate = new Date(inspection.date);
+			var date = inspectionDate.getDate();
+			var month = inspectionDate.getMonth();
+			var year = inspectionDate.getFullYear();
+			
 			return (
 				<div className="Inspection-date">
-					{inspection.date}
+					{month}-{date}-{year}
 				</div>
 			);
 		}
@@ -23,35 +29,48 @@ class Grade extends Component {
 	
 	render() {
 		var inspection = this.props.inspection;
+		var value = (typeof inspection !== 'undefined') ? inspection.grade : this.props.value;
 		var grade;
-		var divStyle;
 
-		switch (this.props.value) {
+		switch (value) {
 			case 'A':
-				grade = <Image src={gradeA} alt="Grade A" />;
+				grade = <Image src={gradeA} alt="Grade A" style={this.props.gradeStyle} />;
 				break;
 
 			case 'B':
-				grade = <Image src={gradeB} alt="Grade B" />;
+				grade = <Image src={gradeB} alt="Grade B" style={this.props.gradeStyle} />;
 				break;
 
 			case 'C':
-				grade = <Image src={gradeC} alt="Grade C" />;
+				grade = <Image src={gradeC} alt="Grade C" style={this.props.gradeStyle} />;
 				break;
 
 			default:
-				grade = <Image src={gradeGP} alt="Grade Pending" />;
+				grade = <Image src={gradeGP} alt="Grade Pending" style={this.props.gradeStyle} />;
 				break;
 		}
 
-		divStyle = {
-			backgroundImage: this.props.seal === 'blue' ? 'url(' + blueSeal + ')' : 'url(' + greySeal + ')'
-		};
+		
 
 		return (
-			<div className={this.props.classNames} style={divStyle}>
+			<div className={this.props.classNames} style={this.props.containerStyle}>
 				{grade}
 				{this.inspectionDate(inspection)}
+			</div>
+		);
+	}
+}
+
+class Address extends Component {
+	render() {
+		return (
+			<div>
+				<div className="street">
+					{this.props.building}&nbsp;{this.props.street}
+				</div>
+				<div className="boro">
+					{this.props.boro},&nbsp;NY&nbsp;{this.props.zipcode}
+				</div>
 			</div>
 		);
 	}
@@ -62,23 +81,71 @@ class Restaurant extends Component {
 		var info = this.props.info;
 		var restaurantStyle = {
 			backgroundImage: 'url(' + info.imageUrl + ')',
-			backgroundRepeat: 'no-repeat'
+			backgroundRepeat: 'no-repeat',
+			backgroundSize: 'cover',
+			height: '200px'
+		};
+
+		var gradeStyle = {
+			width: (function() {
+				var percent = '50%';
+				
+				switch (info.inspections[0].grade) {
+					case 'A':
+						percent = '50%';
+						break;
+					case 'B':
+						percent = '44%';
+						break;
+					case 'C':
+						percent = '50%';
+						break;
+					default:
+						break;
+				}
+
+				return percent;
+			})(),
+			position: 'relative',
+			display: 'block',
+			top: (function() {
+				if (info.inspections[0].grade === 'A' || info.inspections[0].grade === 'B' || info.inspections[0].grade === 'C') {
+					return '10%';
+				} else {
+					return '20%';
+				}
+			})()
+		};
+
+		var containerStyle = {
+			backgroundColor: '#ffffff',
+			backgroundImage: 'url(' + greySeal + ')',
+			backgroundPosition: 'center 15%',
+			backgroundRepeat: 'no-repeat',
+			backgroundSize: '60%',
+			width: '100px',
+			height: '100px',
+			display: 'flex',
+			flexDirection: 'column',
+			alignItems: 'center',
+			position: 'absolute',
+			right: '20px',
+			top: '35%',
+			boxShadow: '0px 2px 2px #ababab'
 		};
 
 		return (
-			<div className="col-sm-3 col-md-6 Restaurant" style={restaurantStyle}>
-				<div className="name">{info.name}</div>
-				<div className="cuisine">{info.cuisine}</div>
-				<div className="address">
-					<div className="street">
-						{info.building}&nbsp;{info.street}
-					</div>
-					<div className="boro">
-						{info.boro},&nbsp;NY&nbsp;{info.zipcode}
-					</div>
+			<div className="col-sm-4">
+				<div style={restaurantStyle}></div>
+				<div className="Restaurant-inspection">
+					<Grade inspection={info.inspections[0]} gradeStyle={gradeStyle} containerStyle={containerStyle}/>
 				</div>
-				<div className="inspection-grade">
-					<Grade inspection={info.inspections[0]} value={info.inspections[0].grade} seal="grey"/>
+				<div className="Restaurant-info">
+					<div className="Restaurant-cuisine">{info.cuisine}</div>
+					<div className="Restaurant-name">{info.name}</div>
+					<div className="Restaurant-address">
+						<Address building={info.building} street={info.street} boro={info.boro} state={info.state} zip={info.state}/>
+					</div>
 				</div>
 			</div>
 		);
@@ -88,16 +155,41 @@ class Restaurant extends Component {
 class SearchForm extends Component {
 	constructor(props) {
 		super(props);
+
+		var gradeStyle = {
+			width: 'auto'
+		};
+
+		var containerStyle = {
+			backgroundColor: '#ffffff',
+			backgroundImage: 'url(' + blueSeal + ')',
+			backgroundPosition: 'center center',
+			backgroundRepeat: 'no-repeat',
+			backgroundSize: '60%',
+			height: '200px',
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center'
+		};
+
 		this.state = {
 			searchFilter: 'all',
 			searchPage: '1',
 			searchQuery: '',
 			searchResults: (
-				<div className="Letter-grades">
-					<Grade value="A" seal="blue" />
-					<Grade value="B" seal="blue" />
-					<Grade value="C" seal="blue" />
-					<Grade value="GP" seal="blue" />
+				<div className="row Letter-grades">
+					<div className="col-sm-3">
+						<Grade value="A" gradeStyle={gradeStyle} containerStyle={containerStyle} />
+					</div>
+					<div className="col-sm-3">
+						<Grade value="B" gradeStyle={gradeStyle} containerStyle={containerStyle} />
+					</div>
+					<div className="col-sm-3">
+						<Grade value="C" gradeStyle={gradeStyle} containerStyle={containerStyle} />
+					</div>
+					<div className="col-sm-3">
+						<Grade value="GP" gradeStyle={gradeStyle} containerStyle={containerStyle}/>
+					</div>
 				</div>
 			)
 		};
@@ -170,9 +262,7 @@ class SearchForm extends Component {
 					</Form>
 				</div>
 				<div className="container Search-results">
-					<div className="row">
-						{this.state.searchResults}
-					</div>
+					{this.state.searchResults}
 				</div>
 			</div>
 		);
@@ -182,7 +272,7 @@ class SearchForm extends Component {
 class Image extends Component {
 	render() {
 		return (
-			<img src={this.props.src} className={this.props.className} alt={this.props.alt} />
+			<img src={this.props.src} className={this.props.className} alt={this.props.alt} style={this.props.style}/>
 		);
 	}
 }
