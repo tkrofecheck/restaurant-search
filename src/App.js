9038ddf5607605e58b9f2133fb6cad4972325b6d
Tim Krofecheck <tkrofecheck@gmail.com>
2017-10-31 12:49:14 -0400

    refactor code for reusability between components

--- src/App.js
+++ src/App.js
@@ -23,16 +23,10 @@ import './App.css';
 const apiKey = 'GC25gGvU068FNzk16wkfN8vK6JmzsKfk6BsYzhpb';
 const gradeLetters = ['A','B','C','GP'];
 
-function formatPhoneNumber(s) {
-	var s2 = (""+s).replace(/\D/g, '');
-	var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
-	return (!m) ? null : "(" + m[1] + ") " + m[2] + "-" + m[3];
-}
-
-class Grade extends Component {
-	inspectionDate(inspection) {
-		if (typeof inspection !== 'undefined') {
-			var inspectionDate = new Date(inspection.date);
+class InspectionDate extends Component {
+	render() {
+		if (this.props.display === 'true') {
+			var inspectionDate = new Date(this.props.date);
 			var date = inspectionDate.getDate();
 			var month = inspectionDate.getMonth() + 1;
 			var year = inspectionDate.getFullYear();
@@ -47,13 +41,18 @@ class Grade extends Component {
 			return (
 				<div className="Inspection-date" data-month={month} data-date={date} data-year={year}></div>
 			);
+		} else {
+			return null;
 		}
 	}
-	
+}
+
+class Grade extends Component {
 	render() {
 		var inspection = this.props.inspection;
-		var value = (typeof inspection !== 'undefined') ? inspection.grade : this.props.value;
+		var value = !_isUndefined(inspection) ? inspection.grade : this.props.value;
 		var grade;
+		var date;
 
 		switch (value) {
 			case 'A':
@@ -73,12 +72,35 @@ class Grade extends Component {
 				break;
 		}
 
-		
+		if (!_isUndefined(inspection)) {
+			date = <InspectionDate date={inspection.date} display={this.props.showDate} />;
+		}
 
 		return (
 			<div className={this.props.classNames} style={this.props.containerStyle}>
 				{grade}
-				{this.inspectionDate(inspection)}
+				{date}
+			</div>
+		);
+	}
+}
+
+class Phone extends Component {
+	constructor(props) {
+		super(props);
+		autoBind(this);
+	}
+	
+	formatPhoneNumber(s) {
+		var s2 = (""+s).replace(/\D/g, '');
+		var m = s2.match(/^(\d{3})(\d{3})(\d{4})$/);
+		return (!m) ? null : "(" + m[1] + ") " + m[2] + "-" + m[3];
+	}
+	
+	render() {
+		return (
+			<div className="phone">
+				{this.formatPhoneNumber(this.props.number)}
 			</div>
 		);
 	}
@@ -91,21 +113,69 @@ class Address extends Component {
 				<div className="address">
 					{this.props.building}&nbsp;{this.props.street},&nbsp;{this.props.boro},&nbsp;NY&nbsp;{this.props.zipcode}
 				</div>
-				<div className="phone">
-					{formatPhoneNumber(this.props.phone)}
-				</div>
+				<Phone number={this.props.phone} />
 			</div>
 		);
 	}
 }
 
-class RestaurantInfo extends Component {
+class RestaurantPhoto extends Component {
 	render() {
-		var data = JSON.stringify(this.props.data);
+		var restaurantStyle = {
+			backgroundImage: 'url(' + this.props.url + ')',
+			backgroundRepeat: 'no-repeat',
+			backgroundSize: 'cover',
+			height: this.props.height
+		};
 		
 		return (
-			<div className="Restaurant-info">
-				{data}
+			<div className="Restaurant-photo" style={restaurantStyle}></div>
+		)
+	}
+}
+
+class RestaurantMoreInfo extends Component {
+	render() {
+		var data = this.props.data;
+		var gradeStyle = {
+			display: 'flex',
+			alignSelf: 'center',
+			justifySelf: 'center',
+			width: '75%',
+			maxWidth: '90px'
+		};
+
+		var containerStyle = {
+			backgroundImage: 'url(' + blueSeal + ')',
+			backgroundPosition: 'center',
+			backgroundRepeat: 'no-repeat',
+			backgroundSize: 'contain',
+			height: '120px',
+			display: 'flex',
+			justifyContent: 'center'
+		};
+		
+		return (
+			<div className="Restaurant-moreinfo">
+				<RestaurantPhoto url={data.imageUrl} height="40%"/>
+				<div className="pad-20">
+					<div className="Restaurant-info">
+						<div className="Restaurant-contact">
+							<div className="Restaurant-name">{data.name}</div>
+							<div className="Restaurant-cuisine">{data.cuisine}-$$$$</div>
+							<div className="Restaurant-address">
+								<Address building={data.building} street={data.street} boro={data.boro} state={data.state} zip={data.state} phone={data.phone}/>
+							</div>
+						</div>
+						<div className="Restaurant-inspection">
+							<Grade inspection={data.inspections[0]} gradeStyle={gradeStyle} containerStyle={containerStyle} showDate="false"/>
+						</div>
+					</div>
+				</div>
+				<div className="Restaurant-inspection-info">
+					<div className="Inspection-heading">Inspections</div>
+					<InspectionDate date={data.inspections[0].date} display="true"/>
+				</div>
 			</div>
 		)
 	}
@@ -142,10 +212,10 @@ class SearchForm extends Component {
 			margin: '0 auto'
 		};
 
-		let initialStateResults = gradeLetters.map(function(letter, index) {
+		let initialStateResults = _map(gradeLetters, function(letter, index) {
 			return (
 				<div key={index} className="col-sm-6 col-md-3">
-					<Grade value={letter} gradeStyle={gradeStyle} containerStyle={containerStyle} />
+					<Grade value={letter} gradeStyle={gradeStyle} containerStyle={containerStyle} showDate="false" />
 				</div>
 			);
 		});
@@ -153,6 +223,7 @@ class SearchForm extends Component {
 		this.state = {
 			gradesBackground: gradesBackground,
 			resultsFound: false,
+			showSearchForm: { visibility: 'visible' },
 			searchBackground: 'rgb(17,61,89)',
 			searchFilter: 'all',
 			searchPage: '1',
@@ -205,8 +276,8 @@ class SearchForm extends Component {
 			modalClass = 'Restaurant-modal';
 			this.setState({ restaurantContent:
 				<div className="Restaurant-details">
-					<div className="close" onClick={(event) => _this.showRestaurantDetails(event, null, false)}></div>
-					<RestaurantInfo data={content} />
+					<div className="close" onClick={(event) => _this.showRestaurantDetails(event, null, false)}>Close</div>
+					<RestaurantMoreInfo data={content} />
 				</div>
 			});
 		} else {
@@ -236,8 +307,6 @@ class SearchForm extends Component {
 			if (this.readyState === 4) {
 				var data = JSON.parse(this.responseText);
 				//console.log('restaurants', data);
-
-				var restaurantStyle;
 				var gradeStyle;
 		
 				var containerStyle = {
@@ -258,13 +327,6 @@ class SearchForm extends Component {
 				};				
 
 				let restaurants = data.map(function(restaurant, index) {
-					restaurantStyle = {
-						backgroundImage: 'url(' + restaurant.imageUrl + ')',
-						backgroundRepeat: 'no-repeat',
-						backgroundSize: 'cover',
-						height: '200px'
-					};
-
 					gradeStyle = {
 						position: 'relative',
 						display: 'block',
@@ -274,9 +336,9 @@ class SearchForm extends Component {
 
 					return (
 						<div key={index} className="col-sm-4" onClick={(event) => _this.showRestaurantDetails(event, restaurant, true)}>
-							<div className="Restaurant-photo" style={restaurantStyle}></div>
+							<RestaurantPhoto url={restaurant.imageUrl} height="200px" />
 							<div className="Restaurant-inspection">
-								<Grade inspection={restaurant.inspections[0]} gradeStyle={gradeStyle} containerStyle={containerStyle}/>
+								<Grade inspection={restaurant.inspections[0]} gradeStyle={gradeStyle} containerStyle={containerStyle} showDate="true" />
 							</div>
 							<div className="Restaurant-info">
 								<div className="Restaurant-name">{restaurant.name}</div>
@@ -289,6 +351,7 @@ class SearchForm extends Component {
 					);
 				});
 
+				_this.setState({ showSearchForm: { visibility: 'hidden' }});
 				_this.setState({ resultsFound: true });
 				_this.setState({ searchResults: _this.wrapRestuarants(restaurants) });
 				_this.setState({ searchBackground: 'url(' + data[data.length-1].imageUrl + ') no-repeat' });
@@ -313,8 +376,8 @@ class SearchForm extends Component {
 		return (
 			<div className="Restaurant-search">
 				<div className="jumbotron Search-form" style={searchFormStyle}>
-					<div className="title">NYC Restaurants</div>
-					<Form inline>
+					<div className="title" style={this.state.showSearchForm}>NYC Restaurants</div>
+					<Form inline style={this.state.showSearchForm}>
 						<DropdownButton bsStyle="default" title="All" id="searchFilter" onSelect={this.handleGradeType}>
 							<MenuItem eventKey="all" active>All</MenuItem>
 							<MenuItem eventKey="a">Grade A</MenuItem>
@@ -334,6 +397,14 @@ class SearchForm extends Component {
 				<Modal content={this.state.restaurantContent} modalClass={this.state.modalClass} />
 			</div>
 		);
+	}
+}
+
+class SearchBox extends Component {
+	render() {
+		return (
+			<div className="Restaurant-searchbox"></div>
+		)
 	}
 }
 
@@ -361,6 +432,7 @@ class App extends Component {
 			<div className="App">
 				<header className="App-header">
 					<Image src={logo} alt="Thomas Logo" />
+					<SearchBox />
 				</header>
 				<SearchForm />
 				<footer className="App-footer">
