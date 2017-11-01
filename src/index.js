3f4f96aefdcf65973ca66590e93606792ce515f3
Tim Krofecheck <tkrofecheck@gmail.com>
2017-11-01 16:58:59 -0400

    add a tricky layout for desktop above 768px

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+if (
+    typeof window.orientation !== 'undefined' ||
+    navigator.userAgent.match(
+        /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i
+    )
+) {
+    document.getElementsByTagName('body')[0].classList.add('mobile');
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
