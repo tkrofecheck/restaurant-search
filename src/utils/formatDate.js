14839d31913d8ccf571139e198d174e15ed389f1
Tim Krofecheck <tkrofecheck@gmail.com>
2017-11-04 09:10:53 -0400

    remove unused var

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -2,7 +2,6 @@ export default function formatDate(d1) {
     var d2 = new Date(d1);
     var date = d2.getDate();
     var month = d2.getMonth();
-    var newDate = null;
     
     if (d1) {
         return {
