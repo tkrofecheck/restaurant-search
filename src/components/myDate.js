715249b78a82577ac79fe16799503d8fc3f63b12
Tim Krofecheck <tkrofecheck@gmail.com>
2017-11-03 21:26:25 -0400

    fix format date function

--- src/components/myDate.js
+++ src/components/myDate.js
@@ -5,12 +5,18 @@ export default class MyDate extends Component {
 	render() {
 		var iDate = formatDate(this.props.date);
 
-		return (
-			<div className="date">
-				<span className="month">{iDate.month}</span>
-				<span className="date-date">{iDate.date}</span>
-				<span className="year">{iDate.year}</span>
-			</div>
-		);
+		if (iDate !== null) {
+			return (
+				<div className="date">
+					<span className="month">{iDate.month}</span>
+					<span className="date-date">{iDate.date}</span>
+					<span className="year">{iDate.year}</span>
+				</div>
+			);
+		} else {
+			return (
+				<div className="date"></div>
+			)
+		}
 	}
 }
--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -2,10 +2,15 @@ export default function formatDate(d1) {
     var d2 = new Date(d1);
     var date = d2.getDate();
     var month = d2.getMonth();
-
-    return {
-        date: (date < 10) ? '0' + date : date,
-        month: (month < 10) ? '0' + month : month,
-        year: d2.getFullYear()
-    };
+    var newDate = null;
+    
+    if (d1) {
+        return {
+            date: (date < 10) ? '0' + date : date,
+            month: (month < 10) ? '0' + month : month,
+            year: d2.getFullYear()
+        }
+    } else {
+        return null;
+    }
 }
